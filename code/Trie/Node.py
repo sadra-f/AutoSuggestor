32e796b9ee8b7d63af391189058def9fc6968548
sadra-f <sadrafouladi@gmail.com>
2024-01-04 21:45:11 +0330

    replace list with numpy array fix bugs

--- code/Trie/Node.py
+++ code/Trie/Node.py
@@ -1,23 +1,26 @@
+import numpy as np
 class Node:
     
-    def __init__(self, value=None, delimited=False, children=[], is_root=False):
+    def __init__(self, value=None, delimited=False, children=np.empty((0,), object), is_root=False):
         self.value = value
         self.is_delimited = delimited
         self.children = children
         self.is_root = is_root
+        if not is_root and self.value == None: raise ValueError
     
     def have_child(self, value):
         try:
-            return True if self.children.index(value) > -1 else False
+            return True if self.child_index(value) > -1 else False
         except ValueError:
             return False
             
-            
+    def append_child(self, value):
+        self.children = np.append(self.children, value)
 
     def child_index(self, value):
         try:
             for i in range(len(self.children)):
-                if self.children[i] == value:
+                if self.children[i].value == value:
                     return i
             return -1
         except ValueError:
@@ -25,4 +28,11 @@ class Node:
         
     @property
     def is_leaf(self):
-        return len(self.children) == 0 and not self.is_root
+        return self.children.size == 0 and not self.is_root
+    
+
+    def __str__(self):
+        return f"{self.value}, {self.children}, {self.is_leaf}"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
