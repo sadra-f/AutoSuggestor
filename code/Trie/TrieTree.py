ed13489fc1fbbc2828aa91fb6f348fc46cc90945
sadra-f <sadrafouladi@gmail.com>
2024-01-05 20:22:44 +0330

    added deletion + some variable rename

--- code/Trie/TrieTree.py
+++ code/Trie/TrieTree.py
@@ -10,10 +10,12 @@ class TrieTree:
 
     def append_word(self, value):
         search_res = self.search(value)
+
         if search_res.exists : return
         elif search_res.processed_chars == value : 
-            search_res.last_node.is_delimited = True
+            search_res.last_node.is_word = True
             return
+        
         current_node = search_res.last_node
         index = len(search_res.processed_chars)
         while index < len(value):
@@ -28,14 +30,30 @@ class TrieTree:
         current_node = self.root
         index = 0
         while not current_node.is_leaf and index < len(value):
-            child_index = current_node.child_index(value[index])
+            child_index = current_node.index_by_value(value[index])
+
             if child_index == -1: return TrieTree.SearchResult(False, value[0:index], current_node)
+
             current_node = current_node.children[child_index]
-            if index == len(value) - 1 and current_node.is_delimited:
+            if index == len(value) - 1 and current_node.is_word:
                 return TrieTree.SearchResult(True, value[0:index+1], current_node)
             index += 1
+
         return TrieTree.SearchResult(False, value[0:index], current_node)
 
+       
+
+    def remove_word(self, value):
+        search_res = self.search(value)
+        if not search_res.exists : return
+        rev_val = value[::-1]
+        current_node = search_res.last_node
+        current_node.is_word = False
+        while not (current_node.is_word or current_node.has_any_child or current_node.is_root) :
+            current_node.parent.remove_child_by_value(rev_val[0])
+            current_node = current_node.parent
+            rev_val = rev_val[1:]
+            
 
 
     class SearchResult:
