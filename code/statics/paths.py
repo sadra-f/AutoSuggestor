f8e56ce94d5649aba714290aeb0ad6aedb032a52
sadra-f <sadrafouladi@gmail.com>
2024-02-15 15:49:29 +0330

    added existing file paths

---
+++ code/statics/paths.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+#================================================================================================
+
+_BETA_NEIGHBORS_STR_PATH = 'statics/neighbors_beta.txt'
+_ALPHA_NEIGHBORS_STR_PATH = 'statics/neighbors_alpha.txt'
+_ALL_NEIGHBORS_STR_PATH = 'statics/neighbors_all.txt'
+_WORD_SET_STR_PATH = '../dataset/words.txt'
+
+#================================================================================================
+
+BETA_NEIGHBORS_PATH = Path(_BETA_NEIGHBORS_STR_PATH)
+ALPHA_NEIGHBORS_PATH = Path(_ALPHA_NEIGHBORS_STR_PATH)
+ALL_NEIGHBORS_PATH = Path(_ALL_NEIGHBORS_STR_PATH)
+WORD_SET_PATH = Path(_WORD_SET_STR_PATH)
