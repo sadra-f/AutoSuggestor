0ea789c52d185386a50398d0fdc18e60997736c4
sadra-f <sadrafouladi@gmail.com>
2024-02-16 16:41:43 +0330

    added path to new dataset

--- code/statics/paths.py
+++ code/statics/paths.py
@@ -4,11 +4,13 @@ from pathlib import Path
 _BETA_NEIGHBORS_STR_PATH = 'code/statics/neighbors_beta.txt'
 _ALPHA_NEIGHBORS_STR_PATH = 'code/statics/neighbors_alpha.txt'
 _ALL_NEIGHBORS_STR_PATH = 'code/statics/neighbors_all.txt'
-_WORD_SET_STR_PATH = 'dataset/words.txt'
+_ALL_WORD_SET_STR_PATH = 'dataset/words.txt'
+_ALPHA_WORD_SET_STR_PATH = 'dataset/words_alpha.txt'
 
 #================================================================================================
 
 BETA_NEIGHBORS_PATH = Path(_BETA_NEIGHBORS_STR_PATH)
 ALPHA_NEIGHBORS_PATH = Path(_ALPHA_NEIGHBORS_STR_PATH)
 ALL_NEIGHBORS_PATH = Path(_ALL_NEIGHBORS_STR_PATH)
-WORD_SET_PATH = Path(_WORD_SET_STR_PATH)
+ALL_WORD_SET_PATH = Path(_ALL_WORD_SET_STR_PATH)
+ALPHA_WORD_SET_PATH = Path(_ALPHA_WORD_SET_STR_PATH)
