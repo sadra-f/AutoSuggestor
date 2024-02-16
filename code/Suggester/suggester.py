c210247cef3bd1048ccb57802c20f0c552f12f9f
sadra-f <sadrafouladi@gmail.com>
2024-02-16 17:05:00 +0330

    can now pickle/unpickle dataset in tree format

--- code/Suggester/suggester.py
+++ code/Suggester/suggester.py
@@ -1,18 +1,21 @@
 from statics.paths import *
 from Trie.TrieTree import TrieTree as TTree
- 
+import pickle
+
+
 class Suggester:
     MODES = ('ALPHA', 'BETA', 'ALL')
-    PATHS = {MODES[0] : ALPHA_NEIGHBORS_PATH, MODES[1] : BETA_NEIGHBORS_PATH, MODES[2] : ALL_NEIGHBORS_PATH}
-    def __init__(self, mode) -> None:
+    NEIGHBOR_PATHS = {MODES[0] : ALPHA_NEIGHBORS_PATH, MODES[1] : BETA_NEIGHBORS_PATH, MODES[2] : ALL_NEIGHBORS_PATH}
+    PICKLE_PATHS = {MODES[0] : PICKLED_ALPHA_WORD_SET_PATH, MODES[1] : PICKLED_ALPHA_WORD_SET_PATH, MODES[2] : PICKLED_WORD_SET_PATH}
+    def __init__(self, mode:str) -> None:
         self.mode = mode
         if mode not in Suggester.MODES:
             raise ValueError(f'mode must be one of the modes specified in the MODES list Options are : {Suggester.MODES}')
-        self._neighbors_path = Suggester.PATHS[self.mode]
+        self._neighbors_path = Suggester.NEIGHBOR_PATHS[self.mode]
         self._neighbors = None
         self.load_raw_neighbors()
         self.word_set = None
-        self.load_raw_dataset()
+        self.load_pickled_datasets()
         print()
 
 
@@ -30,3 +33,12 @@ class Suggester:
         with open(ALL_WORD_SET_PATH, 'r') as file:
             for line in file:
                 self.word_set.append_word(line.strip())
+
+
+    def save_pickled_dataset(self):
+        with open(Suggester.PICKLE_PATHS[self.mode], 'wb') as file:
+            pickle.dump(self.word_set, file, 4)
+    
+    def load_pickled_dataset(self):
+        with open(Suggester.PICKLE_PATHS[self.mode], 'rb') as file:
+            self.word_set = pickle.load(file)
