52703e61cb3e9c90e011383bfa953f1f4da1f401
sadra-f <sadrafouladi@gmail.com>
2024-02-16 17:35:53 +0330

    code refactor for choosing dataset

--- code/Suggester/suggester.py
+++ code/Suggester/suggester.py
@@ -1,19 +1,29 @@
 from statics.paths import *
 from Trie.TrieTree import TrieTree as TTree
+from distance.Levenshtein import LevenshteinDistance as LD
 import pickle
 
 
 class Suggester:
-    MODES = ('ALPHA', 'BETA', 'ALL')
-    NEIGHBOR_PATHS = {MODES[0] : ALPHA_NEIGHBORS_PATH, MODES[1] : BETA_NEIGHBORS_PATH, MODES[2] : ALL_NEIGHBORS_PATH}
-    PICKLE_PATHS = {MODES[0] : PICKLED_ALPHA_WORD_SET_PATH, MODES[1] : PICKLED_ALPHA_WORD_SET_PATH, MODES[2] : PICKLED_WORD_SET_PATH}
-    def __init__(self, mode:str) -> None:
+    
+    SUGGESTION_MODES = ('ALPHA', 'BETA', 'ALL')
+    NEIGHBOR_PATHS = {SUGGESTION_MODES[0] : ALPHA_NEIGHBORS_PATH, SUGGESTION_MODES[1] : BETA_NEIGHBORS_PATH, SUGGESTION_MODES[2] : ALL_NEIGHBORS_PATH}
+    DATASET_MODES = ('ALL', "ALPHA")
+    DATASET_PATHS = {DATASET_MODES[0] : ALL_WORD_SET_PATH, DATASET_MODES[1] : ALPHA_WORD_SET_PATH}
+    PICKLE_PATHS = {DATASET_MODES[0] : PICKLED_ALPHA_WORD_SET_PATH, DATASET_MODES[1] : PICKLED_ALPHA_WORD_SET_PATH}
+    
+    def __init__(self, mode:str, dataset_mode:str) -> None:
+        #TODO : remove load methods and maybe add another method to do that ***
         self.mode = mode
-        if mode not in Suggester.MODES:
-            raise ValueError(f'mode must be one of the modes specified in the MODES list Options are : {Suggester.MODES}')
+        if mode not in Suggester.SUGGESTION_MODES:
+            raise ValueError(f'mode must be one of the modes specified in the MODES list Options are: {Suggester.SUGGESTION_MODES}')
         self._neighbors_path = Suggester.NEIGHBOR_PATHS[self.mode]
         self._neighbors = None
         self.load_raw_neighbors()
+
+        self._dataset_mode = dataset_mode
+        if self._dataset_mode not in Suggester.DATASET_MODES:
+            raise ValueError(f'dataset mode must be one of the modes specified in the DATASET_MODES list, Options are: {Suggester.DATASET_MODES}')
         self.word_set = None
         self.load_pickled_dataset()
         print()
@@ -30,7 +40,7 @@ class Suggester:
     
     def load_raw_dataset(self):
         self.word_set = TTree()
-        with open(ALL_WORD_SET_PATH, 'r') as file:
+        with open(Suggester.DATASET_PATHS[self._dataset_mode], 'r') as file:
             for line in file:
                 self.word_set.append_word(line.strip())
 
@@ -42,3 +52,7 @@ class Suggester:
     def load_pickled_dataset(self):
         with open(Suggester.PICKLE_PATHS[self.mode], 'rb') as file:
             self.word_set = pickle.load(file)
+
+
+    def make_suggestion(self, word):
+        pass
