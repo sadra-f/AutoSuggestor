d84bada84f84ec944a301b78e8851b3466069339
sadra-f <sadrafouladi@gmail.com>
2024-02-16 17:05:18 +0330

    added paths for pickled files

--- code/statics/paths.py
+++ code/statics/paths.py
@@ -7,6 +7,8 @@ _ALL_NEIGHBORS_STR_PATH = 'code/statics/neighbors_all.txt'
 _ALL_WORD_SET_STR_PATH = 'dataset/words.txt'
 _ALPHA_WORD_SET_STR_PATH = 'dataset/words_alpha.txt'
 
+_PICKLED_ALPHA_WORD_SET_STR_PATH =  'dataset/pickled_alpha_words'
+_PICKLED_WORD_SET_STR_PATH =  'dataset/pickled_words'
 #================================================================================================
 
 BETA_NEIGHBORS_PATH = Path(_BETA_NEIGHBORS_STR_PATH)
@@ -14,3 +16,6 @@ ALPHA_NEIGHBORS_PATH = Path(_ALPHA_NEIGHBORS_STR_PATH)
 ALL_NEIGHBORS_PATH = Path(_ALL_NEIGHBORS_STR_PATH)
 ALL_WORD_SET_PATH = Path(_ALL_WORD_SET_STR_PATH)
 ALPHA_WORD_SET_PATH = Path(_ALPHA_WORD_SET_STR_PATH)
+
+PICKLED_ALPHA_WORD_SET_PATH = Path(_PICKLED_ALPHA_WORD_SET_STR_PATH)
+PICKLED_WORD_SET_PATH = Path(_PICKLED_WORD_SET_STR_PATH)
