2b7baa4bf1cb7a4cc48757c9ba0e4af7ec1bb89a
sadra-f <sadrafouladi@gmail.com>
2024-02-17 15:20:35 +0330

    now has a max suggestion value

--- code/Suggester/suggester.py
+++ code/Suggester/suggester.py
@@ -8,9 +8,12 @@ class Suggester:
     
     SUGGESTION_MODES = ('ALPHA', 'BETA', 'ALL')
     NEIGHBOR_PATHS = {SUGGESTION_MODES[0] : ALPHA_NEIGHBORS_PATH, SUGGESTION_MODES[1] : BETA_NEIGHBORS_PATH, SUGGESTION_MODES[2] : ALL_NEIGHBORS_PATH}
+
     DATASET_MODES = ('ALL', "ALPHA")
     DATASET_PATHS = {DATASET_MODES[0] : ALL_WORD_SET_PATH, DATASET_MODES[1] : ALPHA_WORD_SET_PATH}
     PICKLE_PATHS = {DATASET_MODES[0] : PICKLED_WORD_SET_PATH, DATASET_MODES[1] : PICKLED_ALPHA_WORD_SET_PATH}
+
+    MAX_SUGGESTION_COUNT = 8
     
     def __init__(self, mode:str, dataset_mode:str) -> None:
         #TODO : remove load methods and maybe add another method to do that ***
@@ -58,7 +61,7 @@ class Suggester:
         if len(word) < 2: raise ValueError
 
         if self.tt.search(word).exists:
-            return self.tt.search_conditional(word, max_length=len(word)+1)[1:]
+            return self.tt.search_conditional(word, max_length=len(word)+1, max_count=Suggester.MAX_SUGGESTION_COUNT)
         
         #check variations
         suggestions = []
@@ -69,12 +72,11 @@ class Suggester:
                 suggestions.append((tmp_word, LD.calculateDistance(word, tmp_word)[0]))
 
         if len(suggestions) > 0 : 
-            suggestions = sorted(suggestions, key=lambda x: x[1])
+            suggestions = sorted(suggestions, key=lambda x: x[1])[:Suggester.MAX_SUGGESTION_COUNT]
             return suggestions
         
-        min_len = int(len(word)/2)
         tmp_sug = self.tt.search_conditional(word[:-1], min_length= len(word)-1, max_length= len(word) + 1)
         for sug in tmp_sug:
             suggestions.append((sug, LD.calculateDistance(sug, word)[0]))
-        suggestions = sorted(suggestions, key=lambda x: x[1])
+        suggestions = sorted(suggestions, key=lambda x: x[1])[:Suggester.MAX_SUGGESTION_COUNT]
         return suggestions
